ab91179db0c6768a27bd9c2100e7eaabc11ac027
AestyMoee <swa.vincent@gmail.com>
2015-02-03 15:44:10 +0100

    Copie du projet actuel

---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterManager : MonoBehaviour {
+
+	private Animator animStairs, animTrigger;
+
+	public float speedRun = 6.0F;
+	public float speedWalk = 2.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Update() {
+		moveCharacter ();
+	}
+
+	void moveCharacter(){
+		float speed;
+
+		if (Input.GetKey (KeyCode.LeftShift))
+			speed = speedWalk;
+		else
+			speed = speedRun;
+		
+
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+			
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+}
---
+++ Assets/Scripts/NoEuclidManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoEuclidManager : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player")
+			other.transform.position = new Vector3(other.transform.position.x,other.transform.position.y-3, other.transform.position.z);
+	}
+}
