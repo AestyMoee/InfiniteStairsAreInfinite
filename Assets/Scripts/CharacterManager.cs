f495b57e8e0bd916b6283a8bc4fb46c4a0e3da7d
AestyMoee <swa.vincent@gmail.com>
2015-02-03 16:55:35 +0100

    Gestion des pas
    permet de marcher ou courir, joue un son aléatoire à chaque pas

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -4,12 +4,20 @@ using System.Collections;
 public class CharacterManager : MonoBehaviour {
 
 	private Animator animStairs, animTrigger;
+	private AudioSource audioSource;
+
+	public AudioClip[] sndListStep;
 
 	public float speedRun = 6.0F;
 	public float speedWalk = 2.0F;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
+	private float timeBetweenStep;
+
+	void Start(){
+		audioSource = GetComponent<AudioSource> ();
+	}
 
 	void Update() {
 		moveCharacter ();
@@ -22,19 +30,28 @@ public class CharacterManager : MonoBehaviour {
 			speed = speedWalk;
 		else
 			speed = speedRun;
-		
 
-		CharacterController controller = GetComponent<CharacterController>();
+		CharacterController controller = GetComponent<CharacterController> ();
 		if (controller.isGrounded) {
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			if (moveDirection.magnitude > 0.5f && timeBetweenStep < Time.time) { 
+				audioSource.clip = sndListStep [Random.Range (0, sndListStep.Length)];
+				audioSource.Play ();
+
+				if(speed == speedWalk)
+					timeBetweenStep = Time.time + 0.9f;
+				else
+					timeBetweenStep = Time.time + 0.3f;
+
+			}
+			moveDirection = transform.TransformDirection (moveDirection);
 			moveDirection *= speed;
-			if (Input.GetButton("Jump"))
+			if (Input.GetButton ("Jump"))
 				moveDirection.y = jumpSpeed;
-			
+
 		}
 		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
-	}
+		controller.Move (moveDirection * Time.deltaTime);
+		}
 
 }
