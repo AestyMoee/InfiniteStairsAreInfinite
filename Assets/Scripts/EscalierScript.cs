a15c4dc386e551c5eeab613ecf474d1bb00c8adb
AestyMoee <swa.vincent@gmail.com>
2015-03-10 21:39:22 +0100

    Level Design

---
+++ Assets/Scripts/EscalierScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscalierScript : MonoBehaviour {
+
+    private Animator animatedStairs;
+
+	void Start () {
+        animatedStairs = GetComponentInParent<Animator>();
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animatedStairs.Play("EscalierDescente");
+        }
+    }
+}
+ 
